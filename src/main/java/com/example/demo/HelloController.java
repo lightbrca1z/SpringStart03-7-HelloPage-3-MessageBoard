e015b86afb9fb6176e63b55520bc5e230de976c1
michealtateatsu <quwieorpt@gmail.com>
2026-01-20 11:39:39 +0900

    SpringBootで、簡易掲示板を作成しました！

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index(ModelAndView mav) {
+        mav.addObject("msg", "メッセージを入力してください");
+        mav.setViewName("index");
+        return mav;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ModelAndView form(
+            @RequestParam("text1") String str,
+            ModelAndView mav) {
+
+        mav.addObject("msg", "ユーザーのメッセージ：" + str);
+        mav.addObject("value", str);
+        mav.setViewName("index");
+        return mav;
+    }
+}
