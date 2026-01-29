0781818d3ecd58477714c44807257a6ab9a76d37
michealtateatsu <quwieorpt@gmail.com>
2026-01-29 12:08:33 +0900

    フォルダー移動(SpringStart -> SpringStart-Github-Deploy)

---
+++ src/test/java/com/example/demo/SpringStart037HelloPage3MessageBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStart037HelloPage3MessageBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
